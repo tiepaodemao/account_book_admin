e56c269abfca8239302ef292a19baf0f7b94473e
ember <2951148955@qq.com>
2021-05-20 18:15:43 +0800

    第一次提交

---
+++ src/main/java/com/ember/account_book_admin/AccountBookAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ember.account_book_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBookAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ember/account_book_admin/AccountBookAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ember.account_book_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
